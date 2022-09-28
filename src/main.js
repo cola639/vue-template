38c89e215c7fd9e409492f7182ac506a69578e8f
cola639 <337507950@qq.com>
2022-09-28 22:52:22 +0800

    add: elementUI

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from '@/router'
 import store from './store/store'
 
-Vue.config.productionTip = false
+// import ElementUI from 'element-ui'
+import 'normalize.css/normalize.css'
+import '@/styles/index.scss'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.config.productionTip = false
+// Vue.use(ElementUI)
 
 // if (process.env.ENV === 'development') {
 //   const { mockXHR } = require('../mock')
@@ -14,5 +19,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount('#app')
